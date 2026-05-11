6b2917c9e8b5f9ffbff5df2b0eb1261fd82d716a
Sean S <sean.sharifian@gmail.com>
2026-05-10 20:48:03 -0700

    Fix Flask template and app startup

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ import shutil
 import sys
 
 import os
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
 
 import csv
 from datetime import datetime
@@ -230,6 +228,12 @@ def restore_database():
 
 @app.route("/")
 def index():
+
+    return render_template("index.html")
+
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host="0.0.0.0", port=port)
 
     update_membership_status()
 
